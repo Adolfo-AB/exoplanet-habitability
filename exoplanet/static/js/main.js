0b45d441a26205f0ccc74c68db8e08668531c817
Adolfo-AB <adolfoab6@gmail.com>
2023-04-06 17:09:18 +0200

    Enable delete and edit exoplanets

--- exoplanet/static/js/main.js
+++ exoplanet/static/js/main.js
@@ -12,7 +12,80 @@ document.addEventListener("DOMContentLoaded", function() {
 
     exoplanets.forEach(exoplanet => {
       const listItem = document.createElement("li");
-      listItem.textContent = exoplanet.planet_name;
+      const exoplanetLink = document.createElement("a");
+      exoplanetLink.textContent = exoplanet.planet_name;
+      exoplanetLink.href = "";
+
+      exoplanetLink.addEventListener("click", async (event) => {
+        event.preventDefault();
+        contentBlock.innerHTML = "";
+        const response = await fetch(`/exoplanets/${exoplanet.id}/`);
+        const exoplanetDetails = await response.json();
+
+        const exoplanetDetailsList = document.createElement("ul");
+        for (const key in exoplanetDetails) {
+          const listItem = document.createElement("li");
+          listItem.textContent = `${key}: ${exoplanetDetails[key]}`;
+          exoplanetDetailsList.appendChild(listItem);
+        }
+        const deleteButton = document.createElement("button");
+        deleteButton.innerHTML = "delete";
+        deleteButton.addEventListener("click", async (event) => {
+          event.preventDefault();
+          contentBlock.innerHTML = "";
+          const response = await fetch(`/exoplanets/${exoplanet.id}/`, {
+            method: "DELETE",
+            headers: {
+              "Content-Type": "application/json",
+              "X-CSRFToken": csrftoken,
+            }});
+        });
+        const editButton = document.createElement("button");
+        editButton.innerHTML = "edit";
+        editButton.addEventListener("click", async (event) => {
+          event.preventDefault();
+          contentBlock.innerHTML = "";
+
+          const response = await fetch(`/exoplanets/${exoplanet.id}/`);
+          const exoplanetDetails = await response.json();
+
+          const form = document.createElement("form");
+
+          for (const key in exoplanetDetails) {
+            const label = document.createElement("label");
+            label.textContent = key;
+            const input = document.createElement("input");
+            input.name = key;
+            input.value = exoplanetDetails[key];
+            form.appendChild(label);
+            form.appendChild(input);
+          }
+
+          const saveButton = document.createElement("button");
+          saveButton.innerHTML = "save";
+          saveButton.addEventListener("click", async (event) => {
+            event.preventDefault();
+            const formData = new FormData(form);
+            const response = await fetch(`/exoplanets/${exoplanet.id}/`, {
+              method: "PUT",
+              body: formData,
+              headers: {
+                "X-CSRFToken": csrftoken,
+              },
+            });
+            const updatedExoplanet = await response.json();
+            // handle the response as desired
+          });
+
+          form.appendChild(saveButton);
+          contentBlock.appendChild(form);
+        });
+        contentBlock.appendChild(exoplanetDetailsList);
+        contentBlock.appendChild(deleteButton);
+        contentBlock.appendChild(editButton);
+      });
+
+    listItem.appendChild(exoplanetLink);
       exoplanetList.appendChild(listItem);
     });
 
